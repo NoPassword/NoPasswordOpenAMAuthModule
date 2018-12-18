2367fa393b936e6c95cd5e98ae8bcf4789ef7522
migue <migue@manjaro-box>
2018-12-18 11:20:24 -0500

    Updated license text

--- src/test/java/com/nopassword/openam/AuthHelperTest.java
+++ src/test/java/com/nopassword/openam/AuthHelperTest.java
@@ -1,3 +1,21 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2011-2017 ForgeRock AS. All Rights Reserved
+ */
+/**
+ * Portions Copyright 2018 NoPassword Inc.
+ */
 package com.nopassword.openam;
 
 import com.nopassword.openam.module.Constants;
@@ -20,7 +38,6 @@ public class AuthHelperTest {
 
         Assert.assertNotEquals(request.get(Constants.USERNAME).length(), 0);
         Assert.assertEquals(request.get(Constants.API_KEY).length(), 36);
-        Assert.assertEquals(request.get(Constants.COMMAND), Constants.USER_STATUS);
     }
 
     @Test
